9a44fe50e9e53e7d9fa217666cfeb746d69ebd03
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-05-21 02:59:17 +0000

    first commit in js

---
+++ script.js
@@ -0,0 +1,18 @@
+let list []
+let id = 0;
+List = [{},{},...];
+List [0] ->
+{
+    name:"drink coffee",
+    id: 0,
+    done: false,
+    trash: false,
+}
+
+list [1]
+{
+    name:"workout",
+    id:1,
+    done: true,
+    trash: false
+}
