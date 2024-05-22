7f996cdd075cdf8b5c1b5f89fad149b1192c91da
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-05-22 03:54:38 +0000

    added 1 to do and started layout

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = documet.getElementById("list");
+const input = document.getElementById ("input");
+
+const element = document.getElementById ("element")
+element.insertAdjacentHTML(position, text);
+"beforebegin"
+"afterbegin"
+"beforeend"
+"afterend"
+element.innerHTML(text);
+
+const list = document.getElementById("list");
+function addToDo (toDo){
+const text = <li class="item">
+    <i class="co fa fa-circle-thin" job = "complete"></i>
+    <p class="text">${toDo}</p>
+    <i class= "de fa fa-trash-o" job="delete"></i>
+</li>
+
+const position = "beforeend";
+list.insertAdjacentHTML(position,text);
+}
+
+addToDo("Drink Coffee");
+
+//ended 8:20
+
+
+
+
+
 let list []
 let id = 0;
 List = [{},{},...];
