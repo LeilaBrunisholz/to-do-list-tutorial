8e58ef1b919e6c717e5d0d30c9047e0fa51c964a
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-05-22 22:58:58 +0000

    added list functions

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ const input = document.getElementById("input");
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
 const LINE_THROUGH = "lineThrough";
+
+let LIST, id;
+
+let data = localStorage.getItem("TODO");
+
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length; 
+    loadList(LIST); 
+}else{
+   
+    LIST = [];
+    id = 0;
+}
+
+
+function loadList(array){
+    array.forEach(function(item){
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
